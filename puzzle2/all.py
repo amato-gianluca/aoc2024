d36803b8c71e166f69dcb7edb87186a0057ac006
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-12-04 09:27:44 +0100

    Put together the solutions of the two parts of Day 2 puzzle.

--- puzzle2/all.py
+++ puzzle2/all.py
@@ -1,4 +1,11 @@
+"""
+This program solves both parts of Day 2 puzzle.
+"""
+
 def is_safe_with_dampener(l):
+    """
+    Determines whether report l is safe, using the Problem Dampener.
+    """
     if is_safe(l): return True
     for i in range(len(l)):
         l1 = list(l)
@@ -7,6 +14,9 @@ def is_safe_with_dampener(l):
     return False
 
 def is_safe(l):
+    """
+    Determines whether report l is safe, without the use of the Problem Dampener.
+    """
     is_increasing = 1 if l[1] - l[0] > 0 else -1
     for i in range(len(l)-1):
         gap = (l[i+1] - l[i]) * is_increasing
@@ -16,7 +26,11 @@ def is_safe(l):
 
 with open("puzzle2/input") as file:
     count = 0
+    count_dampener = 0
     for line in file:
         l =  [int(x) for x in line.split()]
-        if is_safe_with_dampener(l): count += 1
-print(count)
+        count += is_safe(l)
+        count_dampener += is_safe_with_dampener(l)
+
+print("part 1:", count)
+print("part 2:", count_dampener)
