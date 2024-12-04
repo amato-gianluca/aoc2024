ec05fe03def235417e1efe0fb4a04bc511bc0921
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-12-04 09:20:02 +0100

    Day 3 solutions.

---
+++ puzzle3/all.py
@@ -0,0 +1,72 @@
+"""
+This program solves both parts of the Day 3 puzzle.
+"""
+
+class Computer:
+    """
+    A computer that understands instructions mul, do and don't.
+    """
+
+    def __init__(self):
+        self._total = 0
+        self._guarded_total = 0
+        self._guarded = True
+
+    @property
+    def total(self):
+        """
+        Return the total of all mul instructions, independently from the fact
+        that the computer was in a guarded state or not.
+        """
+        return self._total
+
+    @property
+    def guarded_total(self):
+        """
+        Return the total of all mul instructions executed when the computer
+        was in a guarded state.
+        """
+        return self._guarded_total
+
+    def do(self):
+        """
+        Go to guarded state.
+        """
+        self._guarded = True
+
+    def dont(self):
+        """
+        Exit from guarded state.
+        """
+        self._guarded = False
+
+    def mul(self, a, b):
+        """
+        Multiply `a` and `b`, and update totals.
+        """
+        mul = a * b
+        self._total += mul
+        if self._guarded:
+            self._guarded_total += mul
+
+    def parse(self, s):
+        """
+        Parse the corrupted program `s` in search of instructions.
+        """
+        import re
+        it = re.finditer(r"mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)", s)
+        for m in it:
+            if m[0] == "do()":
+                self.do()
+            elif m[0] == "don't()":
+                self.dont()
+            else:
+                self.mul(int(m[1]), int(m[2]))
+
+computer = Computer()
+with open('puzzle3/input') as file:
+    for line in file:
+        computer.parse(line)
+
+print("part 1:", computer.guarded_total)
+print("part 2:", computer.total)
---
+++ puzzle3/part1.py
@@ -0,0 +1,22 @@
+"""
+This program solves part 1 of Day 3 puzzle.
+"""
+
+def parse(s):
+    """
+    Find the mul instructions in `s` and returns the result of the their
+    execution.
+    """
+    import re
+    total = 0
+    it = re.finditer(r'mul\(([0-9]+),([0-9]+)\)', s)
+    for m in it:
+        total += int(m[1]) * int(m[2])
+    return total
+
+total = 0
+with open('puzzle3/input') as file:
+    for line in file:
+        total += parse(line)
+
+print(total)
