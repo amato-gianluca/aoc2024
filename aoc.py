2c611a0a49a8e7635d1ef22cebff8a2699194266
Gianluca Amato <gianluca.amato.74@gmail.com>
2024-12-20 20:56:00 +0100

    Day 20 solutions.

--- aoc.py
+++ aoc.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 from dataclasses import dataclass, field
 from heapq import heappop, heappush
 from pathlib import Path
-from typing import TextIO
+from typing import Callable, Iterable, TextIO
 
 import __main__
 
@@ -90,3 +90,115 @@ class priority_queue[V]:
                 del self.entry_finder[entry.value]
                 return entry.value
         return None
+
+
+def dijkstra[V](start: V, moves: Callable[[V], Iterable[tuple[V, int]]]) -> tuple[dict[V, V], dict[V, int]]:
+    """
+    The Dijkstra's shortest path algorithm.
+
+    Input parameters:
+      - `start` is the start node
+      - `moves` returns, for each node src, a list of pairs (dst, w) such that dst is reachable from src with weight w
+    Returns:
+      - a map from a node to its predecessor in the shortest path from start
+      - a map from a node n to the shortest distance from start to n
+    """
+    pq = priority_queue[V]()
+    distance: dict[V, int] = {}
+    prev: dict[V, V] = {}
+
+    distance[start] = 0
+    prev[start] = start
+    pq.add(start, 0)
+    while True:
+        node = pq.pop()
+        if node is None:
+            return prev, distance
+        dist = distance[node]
+        for node_new, cost in moves(node):
+            dist_update = dist + cost
+            if node_new in distance:
+                dist_new = distance[node_new]
+                if dist_update < dist_new:
+                    pq.add(node_new, dist_update)
+                    distance[node_new] = dist_update
+                    prev[node_new] = node
+            else:
+                distance[node_new] = dist_update
+                pq.add(node_new, dist_update)
+                prev[node_new] = node
+
+
+def path_compute[V](prevs: dict[V, V], end: V) -> list[V] | None:
+    """
+    Compute a path reaching `end` using the map of predecessor nodes `dict`.
+    """
+    if end not in prevs:
+        return None
+    current = end
+    path = [current]
+    while (p := prevs[current]) != current:
+        path.append(p)
+        current = p
+    path.reverse()
+    return path
+
+
+class Maze:
+    """
+    Class modeling a maze.
+    """
+
+    type position = tuple[int, int]
+
+    def __init__(self, map: list[str]):
+        """
+        Initialize the maze from the map given as a list of strings.
+        """
+        self.map = map
+        self.i_start, self.j_start = self._find_char(self.map, "S")
+        self.i_end, self.j_end = self._find_char(self.map, "E")
+        self.nrow = len(self.map)
+        self.ncol = len(self.map[0])
+
+    @staticmethod
+    def _find_char(map: list[str], ch: str) -> position:
+        """
+        Return the position of a given character in a list of strings.
+        """
+        return [(i, j)
+                for i, line in enumerate(map)
+                for j, obj in enumerate(line)
+                if obj == ch][0]
+
+    def adjacent_positions(self, pos: position) -> list[position]:
+        """
+        Return the list of positions reachable from `pos`.
+        """
+        i, j = pos
+        choices = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+        return [
+            (i_new, j_new)
+            for i_new, j_new in choices
+            if self.map[i_new][j_new] != "#" and 0 <= i_new < self.nrow and 0 <= j_new < self.ncol
+        ]
+
+    def dijkstra(self, start: position | None = None) -> tuple[dict[position, position], dict[position, int]]:
+        """
+        Apply Dijikstra shortes path algorith to the maze, with the given starting node (by default,
+        it is the starting node of the maze).
+        """
+        if start is None:
+            start = (self.i_start, self.j_start)
+        moves: Callable[[Maze.position], list[tuple[Maze.position, int]]] = lambda pos: [
+            (pos_new, 1) for pos_new in self.adjacent_positions(pos)]
+        return dijkstra(start, moves)
+
+    def shortest_path(self) -> list[position] | None:
+        """
+        Return the shortest path from the start to the end node, None if no such a path exists.
+        """
+        return path_compute(self.dijkstra()[0], (self.i_end, self.j_end))
+
+    def __str__(self):
+        return "\n".join("".join(line) for line in self.map)
---
+++ puzzle20/all.py
@@ -0,0 +1,64 @@
+"""
+This program solves both parts of the Day 20 puzzle.
+"""
+
+
+from aoc import *
+
+
+class Day20Maze(Maze):
+    """
+    Extend the Maze class with the support for cheats.
+    """
+
+    def __init__(self, map: list[str]):
+        super().__init__(map)
+
+    def cheats(self, pos: tuple[int, int], steps: int) -> list[Maze.position]:
+        """
+        Return the list of possible cheats starting from `pos` and of length `steps`
+        or less.
+        """
+        i, j = pos
+        return [
+            (i_new, j_new)
+            for i_new in range(i-steps, i+steps+1)
+            for j_new in range(j-steps+abs(i_new - i), j+steps+1-abs(i_new - i))
+            if 0 <= i_new < self.nrow and 0 <= j_new < self.ncol
+            if self.map[i_new][j_new] != "#"
+        ]
+
+    def shortest_path_with_cheat(self, steps: int) -> int | None:
+        """
+        Return the number of cheats of length `steps` or less which makes you
+        gain 100 picoseconds or more.
+        """
+        pos_end = self.i_end, self.j_end
+        preds1, dist1 = self.dijkstra()
+        _, dist2 = self.dijkstra(pos_end)
+        path = path_compute(preds1, pos_end)
+        if path is None:
+            return None
+        length = dist1[pos_end]
+        gains = [0] * length
+        for pos in path:
+            cheats = self.cheats(pos, steps)
+            for cheat_pos in cheats:
+                first_piece = dist1[pos]
+                second_piece = dist2.get(cheat_pos, None)
+                if second_piece is not None:
+                    gain = length - first_piece - second_piece - \
+                        (abs(cheat_pos[0]-pos[0])+abs(cheat_pos[1]-pos[1]))
+                    if gain >= 0:
+                        gains[gain] += 1
+        return sum(gains[100:])
+
+
+def main():
+    content = readfile("input")
+    maze = Day20Maze(content)
+    print("part 1:", maze.shortest_path_with_cheat(2))
+    print("part 2:", maze.shortest_path_with_cheat(20))
+
+
+main()
